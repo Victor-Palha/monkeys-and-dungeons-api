07eb1e4627da2b604cd5a9cea8f2813e1907a3e9
Ash <jv.palha@gmail.com>
2023-03-20 11:55:07 -0300

    Finish letter E spells from PHB

--- src/models/spells/phb-spells.ts
+++ src/models/spells/phb-spells.ts
@@ -2276,6 +2276,252 @@ export const spells:Spells = {
 
             - You instantly light or snuff out a candle, a torch, or a small campfire.`,
             "classes": ["Druid"]
+        },{
+            "name": "Earthquake",
+            "source": "PHB",
+            "level": 8,
+            "ritual": false,
+            "school": "Evocation",
+            "casting_time": "1 action",
+            "range": "500 feet",
+            "components": "V, S, M (a pinch of dirt)",
+            "duration":{
+                "time": "up to 1 minute",
+                "concentration": true,
+            },
+            "description": `
+
+            You create a seismic disturbance at a point on the ground that you can see within range. For the duration, an intense tremor rips through the ground in a 100-foot-radius circle centered on that point and shakes creatures and structures in contact with the ground in that area.
+            
+            The ground in the area becomes difficult terrain. Each creature on the ground that is concentrating must make a Constitution saving throw. On a failed save, the creature's concentration is broken.
+            
+            When you cast this spell and at the end of each turn you spend concentrating on it, each creature on the ground in the area must make a Dexterity saving throw. On a failed save, the creature is knocked prone.
+            
+            This spell can have additional effects depending on the terrain in the area, as determined by the DM.
+
+            Fissures: Fissures open throughout the spell's area at the start of your next turn after you cast the spell. A total of 1d6 such fissures open in locations chosen by the DM. Each is 1d10 × 10 feet deep, 10 feet wide, and extends from one edge of the spell's area to the opposite side. A creature standing on a spot where a fissure opens must succeed on a Dexterity saving throw or fall in. A creature that successfully saves moves with the fissure's edge as it opens.
+            
+            A fissure that opens beneath a structure causes it to automatically collapse (see below).
+
+            Structures: The tremor deals 50 bludgeoning damage to any structure in contact with the ground in the area when you cast the spell and at the start of each of your turns until the spell ends. If a structure drops to 0 hit points, it collapses and potentially damages nearby creatures. A creature within half the distance of a structure's height must make a Dexterity saving throw. On a failed save, the creature takes 5d6 bludgeoning damage, is knocked prone, and is buried in the rubble, requiring a DC 20 Strength (Athletics) check as an action to escape. The DM can adjust the DC higher or lower, depending on the nature of the rubble. On a successful save, the creature takes half as much damage and doesn't fall prone or become buried.`,
+            "classes": ["Cleric", "Druid", "Sorcerer"]
+        },{
+            "name": "Eldritch Blast",
+            "source": "PHB",
+            "level": 0,
+            "ritual": false,
+            "school": "Evocation",
+            "casting_time": "1 action",
+            "range": "120 feet",
+            "components": "V, S",
+            "duration":{
+                "time": "Instantaneous",
+                "concentration": false,
+            },
+            "description": `
+
+            A beam of crackling energy streaks toward a creature within range. Make a ranged spell attack against the target. On a hit, the target takes 1d10 force damage.
+            
+            The spell creates more than one beam when you reach higher levels: two beams at 5th level, three beams at 11th level, and four beams at 17th level. You can direct the beams at the same target or at different ones. Make a separate attack roll for each beam.`,
+            "classes": ["Warlock"]
+        },{
+            "name": "Elemental Weapon",
+            "source": "PHB",
+            "level": 3,
+            "ritual": false,
+            "school": "Transmutation",
+            "casting_time": "1 action",
+            "range": "Touch",
+            "components": "V, S",
+            "duration":{
+                "time": "up to 1 hour",
+                "concentration": true,
+            },
+            "description": `A nonmagical weapon you touch becomes a magic weapon. Choose one of the following damage types: acid, cold, fire, lightning, or thunder. For the duration, the weapon has a +1 bonus to attack rolls and deals an extra 1d4 damage of the chosen type when it hits.`,
+            "higher_level": `When you cast this spell using a spell slot of 5th or 6th level, the bonus to attack rolls increases to +2 and the extra damage increases to 2d4. When you use a spell slot of 7th level or higher, the bonus increases to +3 and the extra damage increases to 3d4.`,
+            "classes": ["Artificer", "Paladin"]
+        },{
+            "name": "Enhance Ability",
+            "source": "PHB",
+            "level": 2,
+            "ritual": false,
+            "school": "Transmutation",
+            "casting_time": "1 action",
+            "range": "Touch",
+            "components": "V, S, M (fur or a feather from a beast)",
+            "duration": {
+                "time": "up to 1 hour",
+                "concentration": true,
+            },
+            "description": `You touch a creature and bestow upon it a magical enhancement. Choose one of the following effects; the target gains that effect until the spell ends.
+            
+            Bear's Endurance: The target has advantage on Constitution checks. It also gains 2d6 temporary hit points, which are lost when the spell ends.
+            
+            Bull's Strength: The target has advantage on Strength checks, and his or her carrying capacity doubles.
+            
+            Cat's Grace: The target has advantage on Dexterity checks. It also doesn't take damage from falling 20 feet or less if it isn't incapacitated.
+            
+            Eagle's Splendor: The target has advantage on Charisma checks.
+            
+            Fox's Cunning: The target has advantage on Intelligence checks.
+
+            Owl's Wisdom: The target has advantage on Wisdom checks.`,
+            "higher_level": "When you cast this spell using a spell slot of 3rd level or higher, you can target one additional creature for each slot level above 2nd.",
+            "classes": ["Artificer", "Bard", "Cleric", "Druid", "Sorcerer"]
+        },{
+            "name": "Enlarge/Reduce",
+            "source": "PHB",
+            "level": 2,
+            "ritual": false,
+            "school": "Transmutation",
+            "casting_time": "1 action",
+            "range": "30 feet",
+            "components": "V, S, M (a pinch of powdered iron)",
+            "duration":{
+                "time": "up to 10 minutes",
+                "concentration": true,
+            },
+            "description": `You cause a creature or an object you can see within range to grow larger or smaller for the duration. Choose either a creature or an object that is neither worn nor carried. If the target is unwilling, it can make a Constitution saving throw. On a success, the spell has no effect.
+
+            If the target is a creature, everything it is wearing and carrying changes size with it. Any item dropped by an affected creature returns to normal size at once.
+            
+            Enlarge: The target's size doubles in all dimensions, and its weight is multiplied by eight. This growth increases its size by one category—from Medium to Large, for example. If there isn't enough room for the target to double its size, the creature or object attains the maximum possible size in the space available. Until the spell ends, the target also has advantage on Strength checks and Strength saving throws. The target's weapons also grow to match its new size. While these weapons are enlarged, the target's attacks with them deal 1d4 extra damage.
+
+            Reduce: The target's size is halved in all dimensions, and its weight is reduced to one-eighth of normal. This reduction decreases its size by one category—from Medium to Small, for example. Until the spell ends, the target also has disadvantage on Strength checks and Strength saving throws. The target's weapons also shrink to match its new size. While these weapons are reduced, the target's attacks with them deal 1d4 less damage (this can't reduce the damage below 1).`,
+            "classes": ["Artificer", "Sorcerer", "Wizard"]
+        },{
+            "name": "Ensnaring Strike",
+            "source": "PHB",
+            "level": 1,
+            "ritual": false,
+            "school": "Conjuration",
+            "casting_time": "1 bonus action",
+            "range": "Self",
+            "components": "V",
+            "duration":{
+                "time": "up to 1 minute",
+                "concentration": true,
+            },
+            "description": `The next time you hit a creature with a weapon attack before this spell ends, a writhing mass of thorny vines appears at the point of impact, and the target must succeed on a Strength saving throw or be restrained by the magical vines until the spell ends. A Large or larger creature has advantage on this saving throw. If the target succeeds on the save, the vines shrivel away.
+
+            While restrained by this spell, the target takes 1d6 piercing damage at the start of each of its turns. A creature restrained by the vines or one that can touch the creature can use its action to make a Strength check against your spell save DC. On a success, the target is freed.`,
+            "higher_level": "If you cast this spell using a spell slot of 2nd level or higher, the damage increases by 1d6 for each slot level above 1st.",
+            "classes": ["Ranger"]
+        },{
+            "name": "Entangle",
+            "source": "PHB",
+            "level": 1,
+            "ritual": false,
+            "school": "Conjuration",
+            "casting_time": "1 action",
+            "range": "90 feet",
+            "components": "V, S",
+            "duration":{
+                "time": "up to 1 minute",
+                "concentration": true,
+            },
+            "description": `Grasping weeds and vines sprout from the ground in a 20-foot square starting from a point within range. For the duration, these plants turn the ground in the area into difficult terrain.
+
+            A creature in the area when you cast the spell must succeed on a Strength saving throw or be restrained by the entangling plants until the spell ends. A creature restrained by the plants can use its action to make a Strength check against your spell save DC. On a success, it frees itself.
+            
+            When the spell ends, the conjured plants wilt away.`,
+            "classes": ["Druid"]
+        },{
+            "name": "Enthrall",
+            "source": "PHB",
+            "level": 2,
+            "ritual": false,
+            "school": "Enchantment",
+            "casting_time": "1 action",
+            "range": "60 feet",
+            "components": "V, S",
+            "duration":{
+                "time": "1 minute",
+                "concentration": false,
+            },
+            "description": `You weave a distracting string of words, causing creatures of your choice that you can see within range and that can hear you to make a Wisdom saving throw. Any creature that can't be charmed succeeds on this saving throw automatically, and if you or your companions are fighting a creature, it has advantage on the save. On a failed save, the target has disadvantage on Wisdom (Perception) checks made to perceive any creature other than you until the spell ends or until the target can no longer hear you. The spell ends if you are incapacitated or can no longer speak.`,
+
+            "classes": ["Bard", "Warlock"]
+        },{
+            "name": "Etheralness",
+            "source": "PHB",
+            "level": 7,
+            "ritual": false,
+            "school": "Transmutation",
+            "casting_time": "1 action",
+            "range": "Self",
+            "components": "V, S",
+            "duration":{
+                "time": "up to 8 hours",
+                "concentration": false,
+            },
+            "description": `You step into the border regions of the Ethereal Plane, in the area where it overlaps with your current plane. You remain in the Border Ethereal for the duration or until you use your action to dismiss the spell. During this time, you can move in any direction. If you move up or down, every foot of movement costs an extra foot. You can see and hear the plane you originated from, but everything there looks gray, and you can't see anything more than 60 feet away.
+
+            While on the Ethereal Plane, you can only affect and be affected by other creatures on that plane. Creatures that aren't on the Ethereal Plane can't perceive you and can't interact with you, unless a special ability or magic has given them the ability to do so.
+            
+            You ignore all objects and effects that aren't on the Ethereal Plane, allowing you to move through objects you perceive on the plane you originated from.
+            
+            When the spell ends, you immediately return to the plane you originated from in the spot you currently occupy. If you occupy the same spot as a solid object or creature when this happens, you are immediately shunted to the nearest unoccupied space that you can occupy and take force damage equal to twice the number of feet you are moved.
+            
+            This spell has no effect if you cast it while you are on the Ethereal Plane or a plane that doesn't border it, such as one of the Outer Planes.`,
+            "higher_level": "When you cast this spell using a spell slot of 8th level or higher, you can target up to three willing creatures (including you) for each slot level above 7th. The creatures must be within 10 feet of you when you cast the spell.",
+            "classes": ["Bard", "Cleric", "Sorcerer", "Warlock", "Wizard"]
+        },{
+            "name": "Evard's Black Tentacles",
+            "source": "PHB",
+            "level": 4,
+            "ritual": false,
+            "school": "Conjuration",
+            "casting_time": "1 action",
+            "range": "90 feet",
+            "components": "V, S, M (a piece of tentacle from a giant octopus or a giant squid)",
+            "duration":{
+                "time": "up to 1 minute",
+                "concentration": true,
+            },
+            "description": `Squirming, ebony tentacles fill a 20-foot square on ground that you can see within range. For the duration, these tentacles turn the ground in the area into difficult terrain.
+
+            When a creature enters the affected area for the first time on a turn or starts its turn there, the creature must succeed on a Dexterity saving throw or take 3d6 bludgeoning damage and be restrained by the tentacles until the spell ends. A creature that starts its turn in the area and is already restrained by the tentacles takes 3d6 bludgeoning damage.
+            
+            A creature restrained by the tentacles can use its action to make a Strength or Dexterity check (its choice) against your spell save DC. On a success, it frees itself.`,
+            "classes": ["Wizard"]
+        },{
+            "name": "Expeditious Retreat",
+            "source": "PHB",
+            "level": 1,
+            "ritual": false,
+            "school": "Transmutation",
+            "casting_time": "1 bonus action",
+            "range": "Self",
+            "components": "V, S",
+            "duration":{
+                "time": "up to 10 minutes",
+                "concentration": true,
+            },
+            "description": `This spell allows you to move at an incredible pace. When you cast this spell, and then as a bonus action on each of your turns until the spell ends, you can take the Dash action.`,
+            "classes": ["Artificer", "Sorcerer", "Warlock", "Wizard"]
+        },{
+            "name": "Eyebite",
+            "source": "PHB",
+            "level": 6,
+            "ritual": false,
+            "school": "Necromancy",
+            "casting_time": "1 action",
+            "range": "Self",
+            "components": "V, S",
+            "duration":{
+                "time": "up to 1 minute",
+                "concentration": true,
+            },
+            "description": `For the spell's duration, your eyes become an inky void imbued with dread power. One creature of your choice within 60 feet of you that you can see must succeed on a Wisdom saving throw or be affected by one of the following effects of your choice for the duration. On each of your turns until the spell ends, you can use your action to target another creature but can't target a creature again if it has succeeded on a saving throw against this casting of eyebite.
+            
+            Asleep: The target falls unconscious. It wakes up if it takes any damage or if another creature uses its action to shake the sleeper awake.
+            
+            Panicked: The target is frightened of you. On each of its turns, the frightened creature must take the Dash action and move away from you by the safest and shortest available route, unless there is nowhere to move. If the target moves to a place at least 60 feet away from you where it can no longer see you, this effect ends.
+            
+            Sickened: The target has disadvantage on attack rolls and ability checks. At the end of each of its turns, it can make another Wisdom saving throw. If it succeeds, the effect ends.`,
+            
+            "classes": ["Bard", "Sorcerer", "Warlock", "Wizard"]
         }
     ]
 }
