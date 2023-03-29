596ff712ca5d502cbbd5841ad93da28edc4c629a
Ash CPC <jv.palha@gmail.com>
2023-03-29 14:35:49 -0300

    Add School to query spells

--- src/controllers/spells/Functions/SpellList.ts
+++ src/controllers/spells/Functions/SpellList.ts
@@ -12,20 +12,22 @@ export function SpellList(spellList:Spell[]){
                 ritual: spell.ritual,
                 concentration: spell.duration.concentration,
                 classes: spell.classes,
+                school: spell.school,
             }
         })
     }
 }
 
 // Função para filtrar os dados
-export function querySpells(spellList: Spell[], {nome, classe, action, concentration, ritual, level}: Query){
+export function querySpells(spellList: Spell[], {nome, classe, action, concentration, ritual, level, school}: Query){
     const filters = [
         (spell) => nome === undefined || spell.name.toLowerCase().includes(nome.toLowerCase()),
         (spell) => classe === undefined || spell.classes.includes(classe),
         (spell) => action === undefined || spell.casting_time.includes(action),
         (spell) => concentration === undefined || spell.duration.concentration === concentration,
         (spell) => ritual === undefined || spell.ritual === ritual,
-        (spell) => level === undefined || spell.level === level
+        (spell) => level === undefined || spell.level === level,
+        (spell) => school === undefined || spell.school === school
     ];
     return spellList.filter(spell => filters.reduce((acc, filter) => acc && filter(spell), true));
 }
--- src/controllers/spells/QuerySpellsController.ts
+++ src/controllers/spells/QuerySpellsController.ts
@@ -8,14 +8,15 @@ export interface Query{
     action:string,
     concentration: boolean,
     ritual: boolean,
-    level: number
+    level: number,
+    school: string
 }
 
 class QuerySpellsController{
     execute(req:Request, res:Response){
-        const { nome, classe, action, concentration, ritual, level} = req.body as Query;
+        const { nome, classe, action, concentration, ritual, level, school} = req.body as Query;
 
-        res.json(SpellList(querySpells(spells.spell, {nome, classe, action, concentration, ritual, level})))
+        res.json(SpellList(querySpells(spells.spell, {nome, classe, action, concentration, ritual, level, school})))
         
     }
 }
