d0306430b38d4cfe761cca429fc737ad49251d93
Ash CPC <jv.palha@gmail.com>
2023-03-29 15:53:41 -0300

    change from body to query

--- src/controllers/spells/QuerySpellsController.ts
+++ src/controllers/spells/QuerySpellsController.ts
@@ -3,18 +3,18 @@ import { spells } from "../../models/spells/spells";
 import { querySpells, SpellList } from "./Functions/SpellList";
 
 export interface Query{
-    nome:string,
-    classe:string,
-    action:string,
-    concentration: boolean,
-    ritual: boolean,
-    level: number,
-    school: string
+    nome?:string,
+    classe?:string,
+    action?:string,
+    concentration?: boolean,
+    ritual?: boolean,
+    level?: number,
+    school?: string
 }
 
 class QuerySpellsController{
     execute(req:Request, res:Response){
-        const { nome, classe, action, concentration, ritual, level, school} = req.body as Query;
+        const { nome, classe, action, concentration, ritual, level, school} = req.query as Query;
 
         res.json(SpellList(querySpells(spells.spell, {nome, classe, action, concentration, ritual, level, school})))
         
